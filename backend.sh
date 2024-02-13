9cae807a2fe2b483e72610e5acf89715eb2557ee
sumanthlearningaem3320 <saisumanthguntur@gmail.com>
2024-02-13 20:52:37 +0530

    test 4

--- backend.sh
+++ backend.sh
@@ -13,10 +13,14 @@ curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl restart backend
+cd /app
+npm install
+
 
 dnf install mysql -y
 
 mysql -h mysql-dev.sumanthguntur320.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+
+systemctl daemon-reload
+systemctl enable backend
+systemctl restart backend
