f87dc2181e7ca0eae76fe8be82be3050b0e15a44
sumanthlearningaem3320 <153350450+sumanthlearningaem3320@users.noreply.github.com>
2024-02-13 08:22:58 +0530

    Initial commit

---
+++ frontend.sh
@@ -0,0 +1,9 @@
+
+dnf install nginx -y
+cp expense.conf  /etc/nginx/default.d/expense.conf
+rm -rf /usr/share/nginx/html/*
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+systemctl enable nginx
+systemctl restart nginx
