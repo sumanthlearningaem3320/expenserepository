bfd46835e7a02b9d5784365e72506056d02ced72
sumanthlearningaem3320 <saisumanthguntur@gmail.com>
2024-02-20 21:54:10 +0530

    frontend.yml

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
 
 dnf install nginx -y
-cp expense.conf  /etc/nginx/default.d/expense.conf
+cp expense.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
